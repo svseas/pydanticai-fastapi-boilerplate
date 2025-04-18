b2a7666cbf851eefd32122192e00ea3dd42035b9
svseas <joe.tang@novobi.com>
2025-04-19 00:01:56 +0700

    update license

--- setup.py
+++ setup.py
@@ -1,30 +1,10 @@
 from setuptools import setup, find_packages
 
+# Most metadata is now defined in pyproject.toml
+# This file is kept for compatibility and to use find_packages
+
 setup(
-    name="pydanticai-fastapi-boilerplate",
-    version="0.1.0",
-    description="A package to generate boilerplate code for PydanticAI, FastAPI, and PostgreSQL stacks.",
-    long_description=open('README.md').read(), # Use the README inside the package
-    long_description_content_type="text/markdown",
-    author="joe.tang",
-    author_email="txt2408@gmail.com",
-    url="https://github.com/svseas", # User's GitHub profile URL
-    packages=find_packages(exclude=["tests*"]), # Automatically find packages
-    install_requires=[
-        "Jinja2>=3.0", # For rendering templates
-    ],
-    entry_points={
-        'console_scripts': [
-            'generate-boilerplate=pydanticai_fastapi_boilerplate.cli:main', # Updated entry point
-        ],
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License", # Choose appropriate license
-        "Operating System :: OS Independent",
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Code Generators",
-    ],
-    python_requires='>=3.8', # Specify minimum Python version
+    packages=find_packages(), # Automatically find packages (like 'pydanticai_fastapi_boilerplate')
+    include_package_data=True, # Include non-code files specified in MANIFEST.in or found by plugins
+    # install_requires is now defined in pyproject.toml under [project.dependencies]
 )
